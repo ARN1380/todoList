71053c10aaced9543dc71dc5292cd4bf5b45c8cd
ARN <alireza.naghavi0@gmail.com>
2023-02-14 11:57:33 +0330

    tasks can only be cheched as done and its just ui - no db or storage or array

--- script.js
+++ script.js
@@ -1,22 +1,55 @@
-let template = document.querySelector("template");
-let toDo = document.querySelector(".todo-list")
+const template = document.querySelector("template");
+const toDo = document.querySelector(".todo-list");
+const input = document.querySelector(".input-text")
 
-// console.log(input);
+// let taskArray = [
+//     {
+//         id: "0",
+//         title: "my task",
+//         dateCreated: "14010510",        
+//         isDone: true
+//     },
+//     {
+//         id: "1",
+//         title: "my second task",
+//         dateCreated: "14010510",        
+//         isDone: false
+//     }
+// ]
 
-let input = document.querySelector(".input-text")
-
-// const item = clone.querySelector(".title");
-
+let taskCount = 0;
 input.addEventListener("keypress", function(event) {
     if (event.key === "Enter") {
-        // event.preventDefault();
-        const clone = template.content.cloneNode(true);
-        const taskText = clone.querySelector(".p-class");
-        taskText.textContent = document.querySelector(".input-text").value;
-        console.log("tasktest: ",taskText);
-
-        console.log(clone);
-        toDo.appendChild(clone);
+       taskCreator();
     }
 });
 
+function taskCreator () {
+    const clone = template.content.cloneNode(true);
+    const taskText = clone.querySelector(".p-class");
+    const item = clone.querySelector(".item")
+    taskText.textContent = document.querySelector(".input-text").value;
+    console.log("tasktest: ",taskText);
+
+    item.id = "task"+taskCount
+    taskCount++;
+
+    console.log(clone);
+    toDo.appendChild(clone);
+}
+
+function doneCheck (e) {
+    let itemClicked = e.srcElement || e.target;
+    document.querySelector("#"+itemClicked.id).classList.add("item-done")        
+    // let isDone = true;
+    // if (isDone) {
+    //     let isDone = false;        
+    // } else {
+    //     let isDone = true;
+    // }
+    // console.log(isDone);
+    // if (isDone) {
+    // } else {
+    //     document.querySelector("#"+itemClicked.id).classList.remove("item-done")        
+    // }
+}
